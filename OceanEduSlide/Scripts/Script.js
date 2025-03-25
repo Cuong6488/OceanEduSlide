b7a229234a480e4d95acc7bf597cbf384ebba5e8
Cuowngf <mmye6789.vn@gmail.com>
2025-03-25 16:05:20 +0700

    4h05 25.3

--- OceanEduSlide/Scripts/Script.js
+++ OceanEduSlide/Scripts/Script.js
@@ -6,10 +6,115 @@
 
 function login() {
     $(".login-box a").on("click", function () {
-    $(".first-login").fadeOut(300, function () { // Ẩn phần tử với hiệu ứng mờ dần
-        $(".second-login").fadeIn(300); // Hiển thị phần tử với hiệu ứng mờ dần
+        $(".first-login").fadeOut(300, function () { // Ẩn phần tử với hiệu ứng mờ dần
+            $(".second-login").fadeIn(300); // Hiển thị phần tử với hiệu ứng mờ dần
+        });
     });
-});
+}
+function about() {
+    $(".btn-map").on("click", function () {
+        $(".about-page > .slide:not(.map-first)").fadeOut(300, function () {
+            $(".map-first").fadeIn(300);
+            $(".map-first").css("display", "flex");
+        });
+    });
+    $(".map-first .btn-map").on("click", function () {
+        $(".offices-number").fadeIn(300);
+        $(".offices-number").css('transform','translateX(0)');
+
+    });
+    $(".btn-north").on("click", function () {
+        $(".map-content-title").fadeOut(0);
+        $(".map-content-title").text('HỆ THỐNG TRUNG TÂM KHU VỰC MIỀN BẮC');
+        $(".map-content-title").fadeIn(300);
+        $(".locations-north").fadeIn(300);
+        $(".locations-mid").fadeOut(300);
+        $(".locations-south").fadeOut(300);
+        $(".btn-mid img").css('width', '20px');
+        $(".btn-south img").css('width', '20px');
+        $(".btn-north img").css('width', '0');
+        $(".offices-number").fadeOut(0);
+        $(".offices-number").css('transform', 'translateX(0)');
+        $(".offices-number").addClass("office-north-text");
+        $(".offices-number").removeClass("office-south-text");
+        $(".offices-number").removeClass("office-mid-text");
+        $(".offices-number").html('Hơn <span>130</span> trung tâm trải dài các tỉnh/ thành phố');
+        $(".offices-number").fadeIn(300);
+        $(".content-slogan").fadeOut(300);
+        //$(".office-list").fadeOut(0);
+        $(".office-list").removeClass("active");
+    });
+    $(".btn-mid").on("click", function () {
+        $(".map-content-title").fadeOut(0);
+        $(".map-content-title").text('HỆ THỐNG TRUNG TÂM KHU VỰC MIỀN TRUNG');
+        $(".map-content-title").fadeIn(300);
+        $(".locations-mid").fadeIn(300);
+        $(".locations-north").fadeOut(300);
+        $(".locations-south").fadeOut(300);
+        $(".btn-north img").css('width', '20px');
+        $(".btn-south img").css('width', '20px');
+        $(".btn-mid img").css('width', '0');
+        $(".offices-number").fadeOut(0);
+        $(".offices-number").css('transform', 'translateX(0)');
+        $(".offices-number").addClass("office-mid-text");
+        $(".offices-number").removeClass("office-south-text");
+        $(".offices-number").removeClass("office-north-text");
+        $(".offices-number").html('Hơn <span>40</span> trung tâm trải dài các tỉnh/ thành phố');
+        $(".offices-number").fadeIn(300);
+        $(".content-slogan").fadeOut(300);
+        //$(".office-list").fadeOut(0);
+        $(".office-list").removeClass("active");
+    });
+    $(".btn-south").on("click", function () {
+        $(".map-content-title").fadeOut(0);
+        $(".map-content-title").text('HỆ THỐNG TRUNG TÂM KHU VỰC MIỀN NAM');
+        $(".map-content-title").fadeIn(300);
+        $(".locations-south").fadeIn(300);
+        $(".locations-north").fadeOut(300);
+        $(".locations-mid").fadeOut(300);
+        $(".btn-north img").css('width', '20px');
+        $(".btn-mid img").css('width', '20px');
+        $(".btn-south img").css('width', '0');
+        $(".offices-number").fadeOut(0);
+        $(".offices-number").css('transform', 'translateX(0)');
+        $(".offices-number").addClass("office-south-text");
+        $(".offices-number").removeClass("office-mid-text");
+        $(".offices-number").removeClass("office-north-text");
+        $(".offices-number").html('Gần <span>10</span> trung tâm tại thành phố Hồ Chí Minh');
+        $(".offices-number").fadeIn(300);
+        $(".content-slogan").fadeOut(300);
+        //$(".office-list").fadeOut(0);
+        $(".office-list").removeClass("active");
+    });
+    $(document).on("click", ".office-south-text", function () {
+        $(".office-list").removeClass("active");
+        $(this).siblings(".south").addClass("active");
+    });
+    $(document).on("click", ".office-mid-text", function () {
+        $(".office-list").removeClass("active");
+        $(this).siblings(".mid").addClass("active");
+    });
+    $(document).on("click", ".office-north-text", function () {
+        $(".office-list").removeClass("active");
+        $(this).siblings(".north").addClass("active");
+    });
+    $(".item").hide();
+    $(".btn-mess").on("click", function () {
+        $(".about-page > .slide:not(.slide-text)").fadeOut(300, function () {
+            $(".slide-text").fadeIn(300);
+            $(".slide-text").css("display", "flex");
+            $(".number span").countUp();
+            $(".item1").show("slow"); 
+
+            setTimeout(function () {
+                $(".item2").show("slow");
+            }, 200);
+
+            setTimeout(function () {
+                $(".item3").show("slow");
+            }, 400);
+        });
+    });
 }
 $("[data-item=city]").on("change", function (data) {
     const id = $(this).val();
