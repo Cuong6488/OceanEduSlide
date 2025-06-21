846d26da05e9cda109be9a0b6474922f4b76acc9
cuong6488 <cuongdv@vico.vn>
2025-06-21 08:10:03 +0700

    fix required

--- OceanEduSlide/ViewModels/AdminViewModel.cs
+++ OceanEduSlide/ViewModels/AdminViewModel.cs
@@ -75,7 +75,7 @@ namespace OceanEduSlide.ViewModels
         [Display(Name = "Chi nhánh"), Required(ErrorMessage = "Hãy chọn chi nhánh")]
         public int OfficeId { get; set; }
         [Display(Name = "Vùng")]
-        public int ZoneId { get; set; }
+        public int? ZoneId { get; set; }
         [Display(Name = "Phân quyền")]
         public TypeUser? TypeUser { get; set; }
         public SelectList SelectOffices { get; set; }
