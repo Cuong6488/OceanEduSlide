a29c2b8616917e47dd9db1b0407278232f9e5d12
cuong6488 <cuongdv@vico.vn>
2026-03-03 17:38:14 +0700

    3.3

---
+++ OceanEduSlide/Migrations/202603020724420_a2.cs
@@ -0,0 +1,24 @@
+﻿namespace OceanEduSlide.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class a2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Debts", "UserOriginId", c => c.Int());
+            AddColumn("dbo.Debts", "EditUser", c => c.Boolean(nullable: false));
+            CreateIndex("dbo.Debts", "UserOriginId");
+            AddForeignKey("dbo.Debts", "UserOriginId", "dbo.Users", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Debts", "UserOriginId", "dbo.Users");
+            DropIndex("dbo.Debts", new[] { "UserOriginId" });
+            DropColumn("dbo.Debts", "EditUser");
+            DropColumn("dbo.Debts", "UserOriginId");
+        }
+    }
+}
